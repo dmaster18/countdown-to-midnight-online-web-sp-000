5ff7381fcafc069bf61222ce32ce2bab81d6f007
David Strauch <dmaster18@gmail.com>
2019-07-23 14:50:44 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -12,6 +12,7 @@ def countdown_with_sleep(integer_argument)
    while (integer_argument > 0 )
     puts "#{integer_argument} SECOND(S)!"
     integer_argument-=1
+    sleep(1)
   end
   "HAPPY NEW YEAR!"
 end
