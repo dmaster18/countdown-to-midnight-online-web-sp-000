0b97e768cc8d948044306d0ec71de3449e0d73a3
David Strauch <dmaster18@gmail.com>
2019-07-23 14:47:26 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -1,7 +1,7 @@
 #write your code here
 integer_argument = 20
 def countdown(integer_argument)
-  while (integer_argument >=0 )
+  while (integer_argument > 0 )
     puts "#{integer_argument} SECOND(S)!"
     integer_argument-=1
   end
